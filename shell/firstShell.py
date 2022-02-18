bf3e16b0bc14f1bcab2c357c6a1635e69dc4c6de
brianperez <secretlyytheflash@gmail.com>
2022-02-18 11:55:19 -0700

    Shell progress

--- shell/firstShell.py
+++ shell/firstShell.py
@@ -1,17 +1,25 @@
 import os
 import re
+import sys
+import time
 
 def runCommand(param):
     rc = os.fork()
     if rc < 0:
-        os._exit(1)
+        sys.exit(1)
     elif rc == 0:
         for dir in re.split(":", os.environ['PATH']):  # try each directory in the path
             program = "%s/%s" % (dir, param[0])
             try:
                 os.execve(program, param, os.environ)  # try to exec program
+                time.sleep(1)
             except FileNotFoundError:  # ...expected
-                pass  # ...fail quietly
+               pass  # ...fail quietly
+            #os.write (1, "Program terminated; Problem unknown")
+        os.write(2, ("Command not found : %s\n" % param[0]).encode())
+        os.write(2, ("Process finished with exit code 1").encode())
+        sys.exit(1)
+        # terminate with error
 
     else:
         os.wait()
@@ -25,11 +33,9 @@ def getInput():
 def main():
     # Don't stop until red light
     while 1:
-        if "PS1" in os.environ:
-            os.write(1, (os.environ["PS1"]).encode())
-        #os.write(1, ('~').encode())
+        os.write(1, (os.environ["PS1"]).encode())
         str = getInput()
-        if str[0] == 'red':
-            os._exit(4)
+        if str[0] == "red":
+            sys.exit(0)
         runCommand(str)
 main()
